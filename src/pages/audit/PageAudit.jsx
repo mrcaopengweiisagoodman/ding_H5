443ac162124031c731c6b3f397c7a9a67c32faae
jiuwanli <2069420645@qq.com>
2019-04-29 16:50:02 +0800

    jiuwanli666
    合同导航页面结构

--- src/pages/audit/PageAudit.jsx
+++ src/pages/audit/PageAudit.jsx
@@ -2,26 +2,46 @@
 require('./PageAudit.less');
 import logic from './PageLogic';
 import { Component, LogicRender } from 'refast';  
+import { 
+    Tabs, 
+    WhiteSpace ,
+    WingBlank,
+    Badge,
+    SearchBar,
+    List
+} from 'antd-mobile';
+// import logic from './PageLogic';
+import {
+    Link
+} from 'react-keeper';
+import mydingready from './../../dings/mydingready';
+const { AUTH_URL , IMGCOMMONURI } = require(`config/develop.json`);
 
 
 class Audit extends Component {
     constructor(props) { 
         super(props, logic);        
-        dd.ready(()=>{
-        	dd.biz.navigation.setTitle({ 
-        		title:'内审' ,
-        		onSuccess: res => {
-        		},
-        		onFail: res => {
-        		}
-        	});
-        })
+        mydingready.ddReady({pageTitle: '内审'});
+        
     }
 
     render() {
         return (
-            <div>
-				内审页面
+            <div className="audit">
+                <Link to={`/auditcontract`} className="box flex_cc1 nsht">
+                    <div className="flex_cc1">
+                        <img src={`${IMGCOMMONURI}nsht.png`} />
+                        <p>内审合同</p>
+                        <span>12</span>
+                    </div>
+                </Link>
+                <Link to={`/auditapprove`} className="box flex_cc1 nssp">
+                    <div className="flex_cc1">
+                        <img src={`${IMGCOMMONURI}nssp.png`} />
+                        <p>内审审批</p>
+                        <span>12</span>
+                    </div>
+                </Link>
             </div>
         );
     }
